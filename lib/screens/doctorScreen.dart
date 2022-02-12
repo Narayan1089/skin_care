bcaef5146dfc357c866ae026f9068e9ff733d510
TusharGharge <tushargharge1111@gmail.com>
2022-02-12 14:46:36 +0530

    doctorscreen

--- lib/screens/doctorScreen.dart
+++ lib/screens/doctorScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:skin_care/utils/colors.dart';
+import 'package:skin_care/widgets/doctorinfo.dart';
 
 class DoctorScreen extends StatefulWidget {
   const DoctorScreen({ Key? key }) : super(key: key);
@@ -10,8 +12,76 @@ class DoctorScreen extends StatefulWidget {
 class _DoctorScreenState extends State<DoctorScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+        shadowColor: Colors.grey,
+        backgroundColor: mobileBackgroundColor,
+        title: const Text(
+          "Doctors",
+          style: TextStyle(
+              fontWeight: FontWeight.bold, color: white, fontSize: 25),
+        ),
+       actions: [IconButton(icon: Icon(Icons.search_outlined,color: white,size: 30.0,),onPressed: (){},)],
+      ),
+      body: Column(children: [Padding(
+        padding: const EdgeInsets.only(top:10,left:20),
+        child: Text('Near you',style:TextStyle(fontSize: 20,color: Colors.grey[400],),),
+      ),
+      
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Container(
+  height: 100,
+  width: double.infinity,
+  decoration: BoxDecoration(
+    color:Colors.black,
+    borderRadius: BorderRadius.only(
+      topLeft: Radius.circular(10),
+          topRight: Radius.circular(10),
+          bottomLeft: Radius.circular(10),
+          bottomRight: Radius.circular(10)
+    ),
+    boxShadow: [
+      BoxShadow(
+          color:Colors.white.withOpacity(0.1),
+          spreadRadius: 5,
+          blurRadius: 7,
+          offset: Offset(0, 3), // changes position of shadow
+      ),
+    ],
+  ),
+  child: Padding(
+    padding: const EdgeInsets.all(12.0),
+    child: Container(
 
+       decoration: BoxDecoration(
+      color:Colors.black,
+      border: Border.all(
+                      color:white,
+                      width: 0.05,
+                    ),
+      ),
+      child: Row(children: [Padding(
+        padding: const EdgeInsets.only(left:12.0),
+        child: CircleAvatar(child: Icon(Icons.mail,),radius: 22,backgroundColor: white,),
+      ),SizedBox(width:MediaQuery.of(context).size.width*0.1,),
+      Padding(
+        padding: const EdgeInsets.only(top:8.0),
+        child: Column(mainAxisAlignment: MainAxisAlignment.start,crossAxisAlignment: CrossAxisAlignment.start,children: [Text("Dr.name of doctor",style:TextStyle(fontSize: 18,),),Text("child specialist",style:TextStyle(fontSize: 15,),),Text("MBBS",style:TextStyle(fontSize: 12,),),],),
+      ),
+      SizedBox(width:MediaQuery.of(context).size.width*0.15),
+       InkWell(child: Icon(Icons.arrow_forward_ios,size: 20,),onTap: (){
+          Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => Doctorinfo()),
+          );
+       },)
+      ],),
+     
+    ),
+  ),
+),
+       ), ],),
     );
   }
 }
--- lib/screens/profileScreen.dart
+++ lib/screens/profileScreen.dart
@@ -13,8 +13,39 @@ class _ProfileScreenState extends State<ProfileScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(elevation: 1,
-        shadowColor: Colors.grey,backgroundColor: mobileBackgroundColor,title: Center(child: Text('Profile',style:TextStyle(color: white,fontSize: 25,fontWeight: FontWeight.bold)))),
-
+        shadowColor: Colors.grey,backgroundColor: mobileBackgroundColor,actions: [Center(child: Text("save",style: TextStyle(fontSize: 18,color: Colors.green[400]),))],title: Center(child: Text('Profile',style:TextStyle(color: white,fontSize: 25,fontWeight: FontWeight.bold),),)),
+      body: Column(children: [Padding(
+        padding: const EdgeInsets.only(top:50),
+        child: Center(child: CircleAvatar(child: Icon(Icons.safety_divider_rounded),radius:70,backgroundColor: white,)),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Container(
+          child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(Icons.person,color: Colors.grey,size:30,),
+          ),Column(crossAxisAlignment: CrossAxisAlignment.start,children: [Text("Name",style: TextStyle(fontSize: 12,color: Colors.grey),),SizedBox(height:2,),Text("Tushar Gharge",style: TextStyle(color:white,fontSize:18),),],),Icon(Icons.edit,color:Colors.green[400],),],),
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Container(
+          child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(Icons.info_outline,color: Colors.grey,size: 30,),
+          ),Column(crossAxisAlignment: CrossAxisAlignment.start,children: [Text("about",style: TextStyle(fontSize: 12,color: Colors.grey),),SizedBox(height:2,),Text("Strong AF",style: TextStyle(color:white,fontSize:18),),],),Icon(Icons.edit,color:Colors.green[400],),],),
+        ),
+      ),Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Container(
+          child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(Icons.person,color: Colors.grey,size: 30,),
+          ),Column(crossAxisAlignment: CrossAxisAlignment.start,children: [Text("contact",style: TextStyle(fontSize: 12,color: Colors.grey),),SizedBox(height:2,),Text("+9167090343",style: TextStyle(color:white,fontSize:18),),],),Icon(Icons.edit,color:Colors.green[400],),],),
+        ),
+      )
+      ],),
     );
+    
   }
 }
